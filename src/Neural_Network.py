33d4b33d54092aa2667d27ac99baede746c37b88
Cody <lukasz.wlodarczyk001@gmail.com>
2020-05-25 05:15:05 +0200

    Add own neural network without optimizer, add example code for ADAM algorithm to app4.0

--- src/Neural_Network.py
+++ src/Neural_Network.py
@@ -1,5 +1,118 @@
-def activation_function(x):
-    if x <=0:
+from random import random
+
+
+# in our case relu = linear
+def relu(x):
+    if x <= 0:
         return 0
     else:
         return x
+
+
+# only for x > 0
+def derivative_relu(x):
+    return 1
+
+
+def adder(inputs, weights):
+    if len(inputs) != len(weights):
+        raise IndexError
+    tmp = 0
+    for x in range(len(inputs)):
+        tmp += inputs[x]*weights[x]
+    return tmp
+
+
+def mean_square_error(output_data, expected_data):
+    error = 0.0
+    elements_in_output = 0
+    for i in range(len(output_data)):
+        for j in range(len(output_data[i])):
+            elements_in_output = len(output_data[i])
+            error += (output_data[i][j]-expected_data[i][j]) * (output_data[i][j]-expected_data[i][j])
+
+    return error/(len(output_data)*elements_in_output)
+
+
+class Neuron:
+    def __init__(self, act_fun):
+        self.inputs = []
+        self.weights = []
+        self.activation_function = act_fun
+
+    def work(self):
+        return self.activation_function(adder(self.inputs, self.weights))
+
+    def work_for_input_layer(self):
+        return self.inputs
+
+    def init_weights(self):
+        for x in range(len(self.inputs)):
+            self.weights.append(random())
+
+
+class Layer:
+    def __init__(self, amount_of_neurons, act_fun, input_layer_flag, previous_layer):
+        self.input_layer_flag = input_layer_flag
+        self.neurons = []
+        for x in range(amount_of_neurons):
+            self.neurons.append(Neuron(act_fun))
+        if not input_layer_flag:
+            self.previous_layer = previous_layer
+
+    def send_output(self):
+        outputs = []
+        for neuron in self.neurons:
+            if not self.input_layer_flag:
+                outputs.append(neuron.work())
+            else:
+                outputs.append(neuron.work_for_input_layer())
+        return outputs
+
+    def load_input_for_first_layer(self, data):
+        for x in range(len(data)):
+            self.neurons[x].inputs = data[x]
+            if len(self.neurons[x].weights) < 1:
+                self.neurons[x].weights.append(1)
+
+    def load_data_from_previous_layer(self):
+        for neuron in self.neurons:
+            neuron.inputs = self.previous_layer.send_output()
+            if len(neuron.weights) == 0:
+                neuron.init_weights()
+
+
+class NeuralNetwork:
+    def __init__(self, amount_of_layers, neurons_in_layer):
+        self.layers = []
+        for i in range(amount_of_layers):
+            if i == 0:
+                self.layers.append(Layer(neurons_in_layer[i],relu,True,None))
+            else:
+                self.layers.append(Layer(neurons_in_layer[i], relu, False, self.layers[-1]))
+
+    def get_output(self):
+        return self.layers[-1].send_output()
+
+    def train(self, inputs):
+        self.layers[0].load_input_for_first_layer(inputs)
+        first = True
+        for layer in self.layers:
+            if first:
+                first = False
+                continue
+            layer.load_data_from_previous_layer()
+
+res =[]
+expected = [[1,2,3,4],[1,2,3,4],[1,2,3,4],[1,2,3,4],[1,2,3,4]]
+siec = NeuralNetwork(3, [4, 4, 4])
+for epochs in range(5):
+    siec.train([1, 2, 3, 4])
+    res.append(siec.get_output())
+print(mean_square_error(res,expected))
+
+
+
+
+
+
--- src/app4.0.py
+++ src/app4.0.py
@@ -1,36 +1,45 @@
-import pandas as pd
+import numpy as np
 
-column_names = ['0/timestamp', 't', 'no', 'measurement x', 'measurement y', 'reference x', 'reference y']
-column_names_test_data = ['0/timestamp', 't', 'no', 'measurement x', 'measurement y', 'reference x', 'reference y',
-                          'błąd pomiaru', 'błąd', 'liczba błędnych próbek', '% błędnych próbek']
-test_data = pd.read_excel('../resources/pozyxAPI_only_localization_dane_testowe_i_dystrybuanta.xlsx')
-test_mes_x = test_data.pop('measurement x')
-test_mes_y = test_data.pop('measurement y')
-test_tar_x = test_data.pop('reference x')
-test_tar_y = test_data.pop('reference y')
-test_mes = pd.concat([test_mes_x,test_mes_y], axis=1)
-test_tar = pd.concat([test_tar_x,test_tar_y], axis=1)
-df1 = pd.read_excel('../resources/pozyxAPI_only_localization_measurement1.xlsx', names=column_names)
-df2 = pd.read_excel('../resources/pozyxAPI_only_localization_measurement2.xlsx', names=column_names)
-df3 = pd.read_excel('../resources/pozyxAPI_only_localization_measurement3.xlsx', names=column_names)
-df4 = pd.read_excel('../resources/pozyxAPI_only_localization_measurement4.xlsx', names=column_names)
-df5 = pd.read_excel('../resources/pozyxAPI_only_localization_measurement5.xlsx', names=column_names)
-df6 = pd.read_excel('../resources/pozyxAPI_only_localization_measurement6.xlsx', names=column_names)
-df7 = pd.read_excel('../resources/pozyxAPI_only_localization_measurement7.xlsx', names=column_names)
-df8 = pd.read_excel('../resources/pozyxAPI_only_localization_measurement8.xlsx', names=column_names)
-df9 = pd.read_excel('../resources/pozyxAPI_only_localization_measurement9.xlsx', names=column_names)
-df10 = pd.read_excel('../resources/pozyxAPI_only_localization_measurement10.xlsx', names=column_names)
-df11 = pd.read_excel('../resources/pozyxAPI_only_localization_measurement11.xlsx', names=column_names)
-df12 = pd.read_excel('../resources/pozyxAPI_only_localization_measurement12.xlsx', names=column_names)
-frames = [df1,df2,df3,df4,df5,df6,df7,df8,df9,df10,df11,df12]
-df = pd.concat(frames, axis=0, ignore_index=True)
-mes_x = df.pop('measurement x')
-mes_y = df.pop('measurement y')
-training_data = pd.concat([mes_x, mes_y], axis=1)
-tar_x = df.pop('reference x')
-tar_y = df.pop('reference y')
-target_data = pd.concat([tar_x, tar_y], axis=1)
-training_data = (training_data.astype('float32') + 2000) / 10000
-target_data = (target_data.astype('float32') + 2000) / 10000
-test_mes = (test_mes.astype('float32') + 2000) / 10000
-test_tar = (test_tar.astype('float32') + 2000) / 10000
+
+def fx2(x):
+    return 2 * x + 3
+
+
+x_range = np.linspace(-1, 1, 100)
+y_value = [fx2(x) for x in x_range]
+
+
+def adam_matrix(inits, X, Y, lr=0.01, n_iter=1, beta1=0.9, beta2=0.999, epsilon=1e-6):
+    n = len(X)
+    a, b = inits
+    grad_func = [lambda x, y: -2 * x * (y - (a * x + b)), lambda x, y: -2 * (y - (a * x + b))]
+    v = np.array([0, 0])
+    s = np.array([0, 0])
+    a_list, b_list = [a], [b]
+    for _ in range(n_iter):
+        t = 1
+        for i in range(n):
+            x_i, y_i = X[i], Y[i]
+            grad = np.array([f(x_i, y_i) for f in grad_func])
+            # compute the first moment
+            v = beta1 * v + (1 - beta1) * grad
+            # compute the second moment
+            s = beta2 * s + (1 - beta2) * (grad ** 2)
+
+            # normalisation
+            v_norm = v / (1 - np.power(beta1, t))
+            s_norm = s / (1 - np.power(beta1, t))
+            t += 1
+
+            # update gradient
+            grad_norm = lr * v_norm / (np.sqrt(s_norm) + epsilon)
+            # update params
+            a -= grad_norm[0]
+            b -= grad_norm[1]
+
+            a_list.append(a)
+            b_list.append(b)
+    return a_list, b_list
+
+print(y_value)
+print(adam_matrix([3,3], x_range, y_value))
